afeee0b8cd18a05444286284338ba8225136d0cd
Chidi-A <chidiace93@gmail.com>
2025-06-03 19:09:46 +0100

    Rename Input component to FormInput to fix case sensitivity issues

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,6 +1,6 @@
 import Form from '../../ui/Form';
 import FormRow from '../../ui/FormRow';
-import Input from '../../ui/Input';
+import Input from '../../ui/FormInput';
 import Spinner from '../../ui/Spinner';
 
 import { useSettings } from './useSettings';
